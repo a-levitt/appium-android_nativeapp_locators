79ca4d6854bc19b15dbed296edd869193cd673ba
a-levitt <artemis.levitt@gmail.com>
2024-07-29 13:07:14 +0200

    Find an element in main menu with AccessibilityID locator

--- src/test/java/alevitt/NativeAppAndroidLocators.java
+++ src/test/java/alevitt/NativeAppAndroidLocators.java
@@ -1,10 +1,19 @@
 package alevitt;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+
 import java.net.MalformedURLException;
 
 
 public class NativeAppAndroidLocators {
     public static void main(String[] args) throws MalformedURLException{
-        DriverInitialization.initializeDriver();
+        AppiumDriver driver = DriverInitialization.initializeDriver();
+
+        WebElement menuElementAccessibility = driver.findElement(AppiumBy.accessibilityId("Accessibility"));
+        menuElementAccessibility.click();
+
+
     }
 }
